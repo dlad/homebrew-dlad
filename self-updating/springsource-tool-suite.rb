75cbb1ea728b759d81eeab12f74d9da4e490b927
Dipen Lad <dipen.lad@gmail.com>
2013-03-31 18:25:41 +0100

    springsource-tool-suite 3.2.0

--- self-updating/springsource-tool-suite.rb
+++ self-updating/springsource-tool-suite.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class SpringsourceToolSuite < Formula
-  url 'http://download.springsource.com/release/STS/3.0.0/dist/e4.2/spring-tool-suite-3.0.0.RELEASE-e4.2-macosx-cocoa-x86_64.tar.gz'
-  version '3.0.0'
-  homepage 'http://www.springsource.com/developer/sts'
-  md5 'b51d4659b8ddf6eb1f64d29c1126b37a'
+  url 'http://download.springsource.com/release/STS/3.2.0/dist/e4.2/spring-tool-suite-3.2.0.RELEASE-e4.2.2-macosx-cocoa-x86_64.tar.gz'
+  version '3.2.0'
+  homepage 'http://www.springsource.com/sts'
+  sha1 'c57d1a193636ddc8f02fd8a9c6cbac9cc43b1d6f'
 
   def shim_script target
     <<-EOS.undent
@@ -14,7 +14,7 @@ class SpringsourceToolSuite < Formula
   end
 
   def install
-    prefix.install Dir['sts-3.0.0.RELEASE/*']
+    prefix.install Dir['sts-3.2.0.RELEASE/*']
     (bin+'sts').write shim_script('STS.app')
   end
 end
